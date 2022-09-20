986095bb19f35c17854a1cac96362114bdc16915
Sophia Zhang <atszhang@gmail.com>
2022-09-19 21:28:25 -0400

    initial solution

--- 739_dailyTemperatures.py
+++ 739_dailyTemperatures.py
@@ -29,7 +29,9 @@ Medium
 Add to List
 
 Share
-Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is 
+the number of days you have to wait after the ith day to get a warmer temperature. 
+If there is no future day for which this is possible, keep answer[i] == 0 instead.
 
  
 
