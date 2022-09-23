4cad64b3cd1dd157110fb15e4d41df3c1e63c810
Sophia Zhang <atszhang@gmail.com>
2022-09-22 23:51:17 -0400

    change to use reversed range

--- 746_minCostClimbingStairs.py
+++ 746_minCostClimbingStairs.py
@@ -4,7 +4,7 @@ from typing import List
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         cost.append(0)
-        for i in range(len(cost) - 3, -1, -1):
+        for i in reversed(range(len(cost) - 3)):
             cost[i] = cost[i] + min(cost[i + 1], cost[i + 2])
         res = min(cost[0], cost[1])
         return res
