4cb3a4aa93afd1ec153c4eaeff26b62f06bd2e84
Sophia Zhang <atszhang@gmail.com>
2022-10-01 20:12:04 -0400

    initial version and rename

---
+++ 678_validParenthesisString.py
@@ -0,0 +1,90 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        # Dynamic Programming: O(n^2)
+        dp = {(len(s), 0): True}  # key=(i, leftCount) -> isValid
+
+        def dfs(i, left):
+            if i == len(s) or left < 0:
+                return left == 0
+            if (i, left) in dp:
+                return dp[(i, left)]
+
+            if s[i] == "(":
+                dp[(i, left)] = dfs(i + 1, left + 1)
+            elif s[i] == ")":
+                dp[(i, left)] = dfs(i + 1, left - 1)
+            else:
+                dp[(i, left)] = (
+                    dfs(i + 1, left + 1) or dfs(i + 1, left - 1) or dfs(i + 1, left)
+                )
+            return dp[(i, left)]
+
+        return dfs(0, 0)
+
+    def checkValidString2(self, s: str) -> bool:
+        # Greedy: O(n)
+        leftMin, leftMax = 0, 0
+
+        for c in s:
+            if c == "(":
+                leftMin, leftMax = leftMin + 1, leftMax + 1
+            elif c == ")":
+                leftMin, leftMax = leftMin - 1, leftMax - 1
+            else:
+                leftMin, leftMax = leftMin - 1, leftMax + 1
+            if leftMax < 0:
+                return False
+            if leftMin < 0:  # required because -> s = ( * ) (
+                leftMin = 0
+        return leftMin == 0
+
+"""
+678. Valid Parenthesis String
+Medium
+
+3866
+
+92
+
+Add to List
+
+Share
+Given a string s containing only three types of characters: '(', ')' and '*', return true if s is valid.
+
+The following rules define a valid string:
+
+Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+Any right parenthesis ')' must have a corresponding left parenthesis '('.
+Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+'*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string "".
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "(*)"
+Output: true
+Example 3:
+
+Input: s = "(*))"
+Output: true
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s[i] is '(', ')' or '*'.
+Accepted
+187,526
+Submissions
+553,985
+"""
+
+
+
+
+
+
