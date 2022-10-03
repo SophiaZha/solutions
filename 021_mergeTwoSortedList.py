813c59ae6dbad8273dcac57946461b09ae0f275e
Sophia Zhang <atszhang@gmail.com>
2022-10-02 23:03:12 -0400

    added a similar solution

--- 021_mergeTwoSortedList.py
+++ 021_mergeTwoSortedList.py
@@ -19,10 +19,27 @@ class Solutionl:
             list2.next = self.mergeTwoLists(list1, list2.next)
             return list2
 
+    def mergeTwoLists2(self, l1, l2):
+        # maintain an unchanging reference to node ahead of the return node.
+        prehead = ListNode(-1)
+        prev = prehead
+        while l1 and l2:
+            if l1.val <= l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+            prev = prev.next
+        # At least one of l1 and l2 can still have nodes at this point, so connect
+        # the non-null list to the end of the merged list.
+        prev.next = l1 if l1 is not None else l2
+
+        return prehead.next
+
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode()
         tail = dummy
-
         while list1 and list2:
             if list1.val < list2.val:
                 tail.next = list1
