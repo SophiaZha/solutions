05fbd50168e254f490b3485e89e714cee1a83e01
Sophia Zhang <atszhang@gmail.com>
2022-10-12 22:58:52 -0400

    add more solutions

--- 287_findTheDuplicateNumber.py
+++ 287_findTheDuplicateNumber.py
@@ -2,6 +2,103 @@ from typing import List
 
 
 class Solution:
+    def findDuplicate1(self, nums: List[int]) -> int:  #not meet requirment, but it works
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                return nums[i]
+
+    def findDuplicate2(self, nums: List[int]) -> int:
+        numset = set()
+        for n in nums:
+            if n in numset:
+                return n
+            else:
+                numset.add(n)
+
+    def findDuplicate3(self, nums: List[int]) -> int:
+        for n in nums:
+            if nums[abs(n) - 1] < 0:
+                return abs(n)
+            else:
+                nums[abs(n) - 1] = -1 * nums[abs(n) - 1]
+
+    def findDuplicate41(self, nums: List[int]) -> int:
+
+        def store(nums: List[int], cur: int) -> int:
+            if cur == nums[cur]:
+                return cur
+            nxt = nums[cur]
+            nums[cur] = cur
+            return store(nums, nxt)
+
+        return store(nums, 0)
+
+    def findDuplicate42(self, nums: List[int]) -> int:
+        while nums[0] != nums[nums[0]]:
+            nums[nums[0]], nums[0] = nums[0], nums[nums[0]]
+        return nums[0]
+
+    def findDuplicate5(self, nums: List[int]) -> int:
+        # 'low' and 'high' represent the range of values of the target
+        low = 1
+        high = len(nums) - 1
+
+        while low <= high:
+            cur = (low + high) // 2
+            count = 0
+
+            # Count how many numbers are less than or equal to 'cur'
+            count = sum(num <= cur for num in nums)
+            if count > cur:
+                duplicate = cur
+                high = cur - 1
+            else:
+                low = cur + 1
+
+        return duplicate
+
+    def findDuplicate6(self, nums: List[int]) -> int:
+        duplicate = 0
+        n = len(nums) - 1
+        bits = n.bit_length()
+        for bit in range(bits):
+            mask = 1 << bit
+            base_count = 0
+            nums_count = 0
+            for i in range(n + 1):
+                # If bit is set in number (i) then add 1 to base_count
+                if i & mask:
+                    base_count += 1
+
+                # If bit is set in nums[i] then add 1 to nums_count
+                if nums[i] & mask:
+                    nums_count += 1
+
+            # If the current bit is more frequently set in nums than it is in
+            # the range [1, 2, ..., n] then it must also be set in the duplicate number.
+            if nums_count - base_count > 0:
+                duplicate |= mask
+
+        return duplicate
+
+    def findDuplicate7(self, nums):
+        # Find the intersection point of the two runners.
+        tortoise = hare = nums[0]
+        while True:
+            tortoise = nums[tortoise]
+            hare = nums[nums[hare]]
+            if tortoise == hare:
+                break
+
+        # Find the "entrance" to the cycle.
+        tortoise = nums[0]
+        while tortoise != hare:
+            tortoise = nums[tortoise]
+            hare = nums[hare]
+
+        return hare
+
     def findDuplicate(self, nums: List[int]) -> int:
         slow, fast = 0, 0
         while True:
