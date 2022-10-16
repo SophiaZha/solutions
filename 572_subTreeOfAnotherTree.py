af65334ee454bace4c6c04e413a077f1feb0a883
Sophia Zhang <atszhang@gmail.com>
2022-10-16 13:45:29 -0400

    added more solutions and details

--- 572_subTreeOfAnotherTree.py
+++ 572_subTreeOfAnotherTree.py
@@ -25,3 +25,25 @@ class Solution:
             if root and subRoot and root.val == subRoot.val:
                 return self.sameTree(root.left, subRoot.left) and self.sameTree(root.right, subRoot.right)
             return False
+
+####################### L20221016 ->
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not root:
+            return False
+        if not subRoot:
+            return True
+        if self.isSameTree(root, subRoot):
+            return True
+
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        else:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
