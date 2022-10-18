99a8aca6f66b734f04522037387ca7824c6f6531
Sophia Zhang <atszhang@gmail.com>
2022-10-18 15:49:32 -0400

    added more solutions and details

--- 021_mergeTwoSortedList.py
+++ 021_mergeTwoSortedList.py
@@ -37,24 +37,19 @@ class Solutionl:
 
         return prehead.next
 
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        tail = dummy
+    def mergeTwoListsL(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(-1)
+        pre = dummy
         while list1 and list2:
             if list1.val < list2.val:
-                tail.next = list1
+                dummy.next = list1
                 list1 = list1.next
             else:
-                tail.next = list2
+                dummy.next = list2
                 list2 = list2.next
-            tail = tail.next
-
-        if list1:
-            tail.next = list1
-        elif list2:
-            tail.next = list2
-
-        return dummy.next
+            dummy = dummy.next
+        dummy.next = list1 if list1 else list2
+        return pre.next
 
     def to_linked_list(self,iterable):
         head = None
