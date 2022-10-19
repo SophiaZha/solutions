aa6ea606be1c32cd33fd1bd9413294582bb927c0
Sophia Zhang <atszhang@gmail.com>
2022-10-19 19:37:47 -0400

    added more solutions and details

--- 023_mergeKSortedLists.py
+++ 023_mergeKSortedLists.py
@@ -51,6 +51,38 @@ class Solution:
             l2.next = self.mergeList(l2.next, l1)
             return l2
 
+######################################################## use PriorityQueue O
+from queue import PriorityQueue
+
+
+class Solution():
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+
+        class Wrapper():
+            def __init__(self, node):
+                self.node = node
+
+            def __lt__(self, other):
+                return self.node.val < other.node.val
+
+        head = point = ListNode(0)
+        q = PriorityQueue()
+        for l in lists:
+            if l:
+                q.put(Wrapper(l))
+        while not q.empty():
+            node = q.get().node
+            point.next = node
+            point = point.next
+            node = node.next
+            if node:
+                q.put(Wrapper(node))
+        return head.next
+
 
 """
 23. Merge k Sorted Lists
