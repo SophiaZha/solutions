950ef59956db4a1d2fedaba1da397f29e24a0f15
Sophia Zhang <atszhang@gmail.com>
2022-10-21 20:41:04 -0400

    added more solutions and details

--- 695_maxIsland.py
+++ 695_maxIsland.py
@@ -24,6 +24,44 @@ class Solution:
             for c in range(COLS):
                 area = max(area, dfs(r, c))
         return area
+
+        # return max(
+        #     dfs(r, c)
+        #     for r in range(rows)
+        #     for c in range(cols)
+        # )
+        #################### 28-32 can replace the row 22-26
+
+##############################L BFS
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        visited = set()
+        rows, cols = len(grid), len(grid[0])
+        max_area = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] and (r, c) not in visited:
+                    stack = [(r, c)]
+                    visited.add((r, c))
+                    area = 0
+
+                    while stack:
+                        (x, y) = stack.pop()
+                        area += 1
+                        for (m, n) in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
+                            if (m in range(rows) and
+                                    n in range(cols) and
+                                    grid[m][n] and
+                                    (m, n) not in visited
+                            ):
+                                stack.append((m, n))
+                                visited.add((m, n))
+                    max_area = max(max_area, area)
+
+        return max_area
+
+
 """
 695. Max Area of Island
 Medium
