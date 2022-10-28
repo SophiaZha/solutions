0241aaa13ff8cab0f6dc6891d91e0c52136f4b19
Sophia Zhang <atszhang@gmail.com>
2022-10-27 20:03:39 -0400

    added more solution

--- 239_slidingWindowMax.py
+++ 239_slidingWindowMax.py
@@ -20,7 +20,20 @@ class Solution:
                 l += 1
             r +=1
         return output
-
+###########
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        output = []
+        q = deque()
+        for i, cur in enumerate(nums):
+            while q and nums[q[-1]] <= cur:
+                q.pop()
+            q.append(i)
+            if i - k == q[0]:
+                q.popleft()
+            if i >= k - 1:
+                output.append(nums[q[0]])
+        return output
 ##################
 from collections import deque
 
