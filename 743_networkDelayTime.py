47eba374c1317ef98b72ea9474b7097934a3092d
Sophia Zhang <atszhang@gmail.com>
2022-10-28 22:25:14 -0400

    added more solution

--- 743_networkDelayTime.py
+++ 743_networkDelayTime.py
@@ -1,14 +1,11 @@
 import collections
 import heapq
 from typing import List
-
-
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         edges = collections.defaultdict(list)
         for u, v, w in times:
             edges[u].append((v, w))
-
         minHeap = [(0, k)]
         visit = set()
         t = 0
@@ -16,16 +13,19 @@ class Solution:
             w1, n1 = heapq.heappop(minHeap)
             if n1 in visit:
                 continue
+            t = w1
             visit.add(n1)
-            t = max(t, w1)
-
             for n2, w2 in edges[n1]:
                 if n2 not in visit:
                     heapq.heappush(minHeap, (w1 + w2, n2))
         return t if len(visit) == n else -1
 
         # O(E * logV)
-
+times = [[2,1,1],[2,3,1],[3,4,1]]
+n = 4
+k = 2
+so = Solution()
+print(so.networkDelayTime(times, n, k))
 """
 743. Network Delay Time
 Medium
