4e05a3d4875705abf4e97970777f5cef090a9be2
Sophia Zhang <atszhang@gmail.com>
2022-10-31 15:08:42 -0400

    added more solution

--- 070_climbingStairs.py
+++ 070_climbingStairs.py
@@ -18,6 +18,15 @@ class Solution:
             n2, n1 = n1 + n2 , n2
         return n2
 
+    def climbStairs3(self, n: int) -> int:
+        if n <=2:
+            return n
+        dp = [0] *(n+1)
+        dp[1], dp[2] = 1, 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+
 n = 4
 x = Solution()
 print(x.climbStairs(n))
