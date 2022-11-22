9f2ea67bd368ec2676f60a09cf38e128b5037d2f
Sophia Zhang <atszhang@gmail.com>
2022-11-22 14:55:50 -0500

    minor update

--- 062_uniquePaths.py
+++ 062_uniquePaths.py
@@ -8,6 +8,14 @@ class Solution:
             row = newRow
         return row[0]
 
+    def uniquePathsO(self, m: int, n: int) -> int:
+        d = [[1] * n for _ in range(m)]
+
+        for col in range(1, m):
+            for row in range(1, n):
+                d[col][row] = d[col - 1][row] + d[col][row - 1]
+
+        return d[m - 1][n - 1]
 
 """
 62. Unique Paths
@@ -20,7 +28,8 @@ Medium
 Add to List
 
 Share
-There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). 
+There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). 
+The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). 
 The robot can only move either down or right at any point in time.
 
 Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
