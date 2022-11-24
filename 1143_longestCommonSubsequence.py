9fb0d6346a47421c07a708d270968beee94f169d
Sophia Zhang <atszhang@gmail.com>
2022-11-24 09:27:34 -0500

    more solutions

--- 1143_longestCommonSubsequence.py
+++ 1143_longestCommonSubsequence.py
@@ -1,6 +1,5 @@
 from functools import lru_cache
 from collections import defaultdict
-
 class Solution: #L
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         dp = [[0 for x in range(len(text2) + 1)] for y in range(len(text1) + 1)]
@@ -90,6 +89,30 @@ class Solution: #L
         return dp_grid[0][0]
 
 
+    def longestCommonSubsequenceO4(self, text1: str, text2: str) -> int:
+
+        # If text1 doesn't reference the shortest string, swap them.
+        if len(text2) < len(text1):
+            text1, text2 = text2, text1
+
+        # The previous and current column starts with all 0's and like
+        # before is 1 more than the length of the first word.
+        previous = [0] * (len(text1) + 1)
+        current = [0] * (len(text1) + 1)
+
+        # Iterate up each column, starting from the last one.
+        for col in reversed(range(len(text2))):
+            for row in reversed(range(len(text1))):
+                if text2[col] == text1[row]:
+                    current[row] = 1 + previous[row + 1]
+                else:
+                    current[row] = max(previous[row], current[row + 1])
+            # The current column becomes the previous one, and vice versa.
+            previous, current = current, previous
+
+        # The original problem's answer is in previous[0]. Return it.
+        return previous[0]
+
 sol = Solution()
 
 text1 = "abcba"
@@ -116,7 +139,8 @@ Add to List
 Share
 Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
 
-A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+A subsequence of a string is a new string generated from the original string with some characters (can be none)
+ deleted without changing the relative order of the remaining characters.
 
 For example, "ace" is a subsequence of "abcde".
 A common subsequence of two strings is a subsequence that is common to both strings.
