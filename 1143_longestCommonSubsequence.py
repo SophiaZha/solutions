61b7cd9b63df02b9f9b389246947b60eb596e282
Sophia Zhang <atszhang@gmail.com>
2022-11-25 19:36:14 -0500

    minor update

--- 1143_longestCommonSubsequence.py
+++ 1143_longestCommonSubsequence.py
@@ -55,11 +55,8 @@ class Solution: #L
                 return 0
             if text1[p1] == text2[p2]:
                 return 1 + memo_solve(p1 + 1, p2 + 1)
-
-            # Recursive case 2.
             else:
                 return max(memo_solve(p1, p2 + 1), memo_solve(p1 + 1, p2))
-
         return memo_solve(0, 0)
 
     def longestCommonSubsequenceO3(self, text1: str, text2: str) -> int:
