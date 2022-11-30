4576edb2f1f7650f4b714c9d6bb2f0b68f9f71cb
Sophia Zhang <atszhang@gmail.com>
2022-11-30 17:00:20 -0500

    minor update

--- 0.py
+++ 0.py
@@ -3,6 +3,70 @@ import heapq
 from collections import defaultdict, Counter
 import operator
 import math
+from collections import OrderedDict
+
+print("This is a Dict:\n")
+d = {}
+d['a'] = 1
+d['b'] = 2
+d['c'] = 3
+d['d'] = 4
+
+for key, value in d.items():
+	print(key, value)
+
+print("\nThis is an Ordered Dict:\n")
+od = OrderedDict()
+od['a'] = 1
+od['b'] = 2
+od['c'] = 3
+od['d'] = 4
+# A Python program to demonstrate working of key
+# value change in OrderedDict
+from collections import OrderedDict
+
+print("Before:\n")
+od = OrderedDict()
+od['a'] = 1
+od['b'] = 2
+od['c'] = 3
+od['d'] = 4
+for key, value in od.items():
+	print(key, value)
+
+print("\nAfter:\n")
+od['c'] = 5
+for key, value in od.items():
+	print(key, value)
+
+for key, value in od.items():
+	print(key, value)
+
+# A Python program to demonstrate working of deletion
+# re-insertion in OrderedDict
+from collections import OrderedDict
+
+print("Before deleting:\n")
+od = OrderedDict()
+od['a'] = 1
+od['b'] = 2
+od['c'] = 3
+od['d'] = 4
+
+for key, value in od.items():
+	print(key, value)
+
+print("\nAfter deleting:\n")
+od.pop('c')
+for key, value in od.items():
+	print(key, value)
+
+print("\nAfter re-inserting:\n")
+od['c'] = 3
+for key, value in od.items():
+	print(key, value)
+
+
 
 print(Counter("mississippi"))
 print("String as input   :",  end='')
--- 494_targetSum.py
+++ 494_targetSum.py
@@ -4,30 +4,25 @@ from typing import List
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         dp = {}  # (index, total) -> # of ways
-
         def backtrack(i, total):
             if i == len(nums):
                 return 1 if total == target else 0
             if (i, total) in dp:
                 return dp[(i, total)]
-
             dp[(i, total)] = backtrack(i + 1, total + nums[i]) + backtrack(
                 i + 1, total - nums[i]
             )
             return dp[(i, total)]
-
         return backtrack(0, 0)
 
     def findTargetSumWaysL(self, nums: List[int], target: int) -> int:
         dp = {}  # index, currentTotal
-
         def backTrack(i, currentTotal):
             if i == len(nums) - 1:
                 if currentTotal == target:
                     return 1
                 else:
                     return 0
-
             if (i, currentTotal) in dp:
                 return dp[(i, currentTotal)]
 
