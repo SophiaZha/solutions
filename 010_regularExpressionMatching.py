eb56e3b5cf0f2fb1741a0adc9e69fc03decfd6b8
Sophia Zhang <atszhang@gmail.com>
2022-12-13 17:57:55 -0500

    more solutions

--- 010_regularExpressionMatching.py
+++ 010_regularExpressionMatching.py
@@ -42,6 +42,17 @@ class Solution: #L
 
         return dfs(0, 0)
 
+    def isMatch0(self, text, pattern):
+        if not pattern:
+            return not text
+
+        first_match = bool(text) and pattern[0] in {text[0], '.'}
+
+        if len(pattern) >= 2 and pattern[1] == '*':
+            return (self.isMatch(text, pattern[2:]) or
+                    first_match and self.isMatch(text[1:], pattern))
+        else:
+            return first_match and self.isMatch(text[1:], pattern[1:])
     """
     10. Regular Expression Matching
 Hard
