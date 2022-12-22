ca91218fbdf6de121e29944fa2b7f7083146c072
Sophia Zhang <atszhang@gmail.com>
2022-12-22 11:08:07 -0500

    added solution

--- 134_gasStation.py
+++ 134_gasStation.py
@@ -4,7 +4,6 @@ class Solution:  # L
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         if sum(gas) < sum(cost):
             return -1
-
         total = 0
         res = 0
         for i in range(len(gas)):
@@ -28,6 +27,28 @@ class Solution:  # L
             total += gas[end] - cost[end]
             end += 1
         return -1
+
+    def canCompleteCircuitO(self, gas, cost):
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        n = len(gas)
+
+        total_tank, curr_tank = 0, 0
+        starting_station = 0
+        for i in range(n):
+            total_tank += gas[i] - cost[i]
+            curr_tank += gas[i] - cost[i]
+            # If one couldn't get here,
+            if curr_tank < 0:
+                # Pick up the next station as the starting one.
+                starting_station = i + 1
+                # Start with an empty tank.
+                curr_tank = 0
+
+        return starting_station if total_tank >= 0 else -1
 
     """
     134. Gas Station
