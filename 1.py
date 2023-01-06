cc71c14aaf9789fc6eae7d1e700d5c7e1b43d854
Sophia Zhang <atszhang@gmail.com>
2023-01-06 16:10:45 -0500

    minor update

--- 1.py
+++ 1.py
@@ -9,3 +9,7 @@ def sum_of_elements(*arg):
 
 result = sum_of_elements(*[1, 2, 3, 4])
 print(result)  # 10
+
+#5. Get all the elements in the middle of the list
+_, *elements_in_the_middle, _ = [1, 2, 3, 4, 5, 6, 7, 8]
+print(elements_in_the_middle)  # [2, 3, 4, 5, 6, 7]
