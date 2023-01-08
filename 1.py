b61e00ef0e6064e104ad35ef86eebb1ebadc9b32
Sophia Zhang <atszhang@gmail.com>
2023-01-08 18:40:35 -0500

    minor update

--- 1.py
+++ 1.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 my_list = [1, 2, 3, 4]
 print(my_list)  # [1, 2, 3, 4]
 print(*my_list)  # 1 2 3 4
@@ -17,3 +19,16 @@ print(elements_in_the_middle)  # [2, 3, 4, 5, 6, 7]
 numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 even_numbers = [number for number in numbers if number % 2 == 0]
 print(even_numbers)  # [2, 4, 6, 8]
+
+class Status(Enum):
+    NO_STATUS = -1
+    NOT_STARTED = 0
+    IN_PROGRESS = 1
+    COMPLETED = 2
+
+
+print(Status.IN_PROGRESS.name)  # IN_PROGRESS
+print(Status.COMPLETED.value)  # 2
+string = "Abc"
+
+print(string * 5)  # AbcAbcAbcAbcAbc
