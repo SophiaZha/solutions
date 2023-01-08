c27b877e927e46b126a21af82a2a19a0d7f8c5cc
Sophia Zhang <atszhang@gmail.com>
2023-01-07 19:54:35 -0500

    minor update

--- 1.py
+++ 1.py
@@ -13,3 +13,7 @@ print(result)  # 10
 #5. Get all the elements in the middle of the list
 _, *elements_in_the_middle, _ = [1, 2, 3, 4, 5, 6, 7, 8]
 print(elements_in_the_middle)  # [2, 3, 4, 5, 6, 7]
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+even_numbers = [number for number in numbers if number % 2 == 0]
+print(even_numbers)  # [2, 4, 6, 8]
