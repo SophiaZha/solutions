2556b70ca50bc69eb916aed67d743584afdbf0c4
Sophia Zhang <atszhang@gmail.com>
2023-01-09 20:02:45 -0500

    minor update

---
+++ blueFilter.py
@@ -0,0 +1,33 @@
+
+
+
+
+"""
+In this exercise, you’ll get some more practice writing image filters!
+
+YOUR JOB:
+Write a function called remove_blue that takes an image as a parameter and sets the blue value of every pixel in the image to be 0. This will remove all blue from the image, giving it a night vision tone.
+
+EXAMPLE:
+
+BEFORE:
+
+
+AFTER:
+
+
+BONUS CHALLENGE:
+Try writing extra filters like remove_red that sets all red values to 0,
+and remove_green which sets all green values to 0.
+
+What happens if you only cut the blue values in half instead of setting them
+to 0? What happens if you remove both red and blue, or both red and green? See what kinds of cool filters you can come up with!
+
+See if you can produce this image:
+
+
+Or this image:
+
+
+
+"""
