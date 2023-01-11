560fde7b0e8adcb9f4ff67693ff5b1086af087c3
Sophia Zhang <atszhang@gmail.com>
2023-01-10 20:13:20 -0500

    minor update

--- blueFilter.py
+++ blueFilter.py
@@ -1,3 +1,26 @@
+from tkinter import Image
+
+IMAGE_URL = "https://codehs.com/uploads/c709d869e62686611c1ac849367b3245"
+IMAGE_WIDTH = 280
+IMAGE_HEIGHT = 200
+
+RED = 0
+GREEN = 1
+BLUE = 2
+def remove_green_and_blue(pixel):
+    new_green = 0
+    new_blue = 0
+    return (pixel[RED], new_green, new_blue)
+
+image = Image(IMAGE_URL)
+image.set_position(70, 70)
+image.set_size(IMAGE_WIDTH, IMAGE_HEIGHT)
+add(image)
+
+
+
+
+
 
 
 
@@ -6,7 +29,8 @@
 In this exercise, you’ll get some more practice writing image filters!
 
 YOUR JOB:
-Write a function called remove_blue that takes an image as a parameter and sets the blue value of every pixel in the image to be 0. This will remove all blue from the image, giving it a night vision tone.
+Write a function called remove_blue that takes an image as a parameter and sets the blue value of every pixel in the image to be 0.
+his will remove all blue from the image, giving it a night vision tone.
 
 EXAMPLE:
 
---
+++ darkenFilter.py
@@ -0,0 +1,32 @@
+
+
+"""
+In this exercise we’ll get some practice modifying pixels with the Image class.
+
+YOUR JOB:
+Write a darken_filter and change_picture function that modifies the pixels in the left half of the image to be darker.
+
+You can darken a pixel by subtracting the same value from each of the red, green, and blue values of the pixel.
+
+for every pixel in the left half of the image
+    set pixel red to be (red - DARKENING_FACTOR)
+    set pixel green to be (green - DARKENING_FACTOR)
+    set pixel blue to be (blue - DARKENING_FACTOR)
+Of course, we cannot have negative values for red, green, or blue, so make sure to limit the pixel values to not go below 0.
+
+EXAMPLE:
+
+BEFORE:
+
+
+AFTER:
+
+
+HINT:
+This exercise is very similar to the previous example Brighten Filter. The main difference is,
+instead of adding the same value to each pixel, we are subtracting the same value from each pixel. We are also only applying this on half the picture.
+
+Looking at the Brighten Filter example and really understanding that code is a good starting place for writing the Darken Filter.
+
+
+"""
