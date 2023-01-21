704e659792f66e31aace87ca4d31c7195250a171
Sophia Zhang <atszhang@gmail.com>
2023-01-21 16:07:45 -0500

    minor update

--- tt/dustControl.py
+++ tt/dustControl.py
@@ -1,20 +1,20 @@
 import sys
 workers, streets = 0, 0
-tasks = []
+jobs = []
 for i, line in enumerate(sys.stdin):
     input = line.split(" ")
     n, m = int(input[0]), int(input[1])
     if i == 0:
         workers, streets = n, m
-        tasks = [0 for i in range(m + 1)]
+        jobs = [0 for i in range(m + 1)]
     else:
-        tasks[n] += 1
-        tasks[m+1] -= 1
+        jobs[n] += 1
+        jobs[m + 1] -= 1
 
 ret = 0
 s = 0
-for i in range(len(tasks) - 1):
-    s += tasks[i]
+for i in range(len(jobs) - 1):
+    s += jobs[i]
     if s == 0:
         ret += 1
 print(ret)
