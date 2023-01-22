5fbab0ed61471d07a7b98682b497f9f4c185b656
Sophia Zhang <atszhang@gmail.com>
2023-01-21 19:24:41 -0500

    more solution

--- tt/shortestTimeTrip.py
+++ tt/shortestTimeTrip.py
@@ -1,3 +1,5 @@
+# https://leetcode.com/problems/network-delay-time/
+# https://www.youtube.com/watch?v=EaphyqKU4PQ
 import collections
 import heapq
 def get_shortest_time(startNode, endNode, paths):
@@ -8,21 +10,30 @@ def get_shortest_time(startNode, endNode, paths):
         visited = set()
         time_dict = {}
         time_dict[startNode] = 0
-
-        while heap:
-            curr_time, curr_loc = heapq.heappop(heap)
-            if curr_loc not in visited:
-                visited.add(curr_loc)
-                if curr_loc == endNode:
-                    return curr_time
-                for time, to_loc in routes.get(curr_loc, ()):
-                    if to_loc in visited:
-                        continue
-                    prev = time_dict.get(to_loc, None)
-                    new_time = curr_time + time
-                    if prev is None or new_time < prev:
-                        time_dict[to_loc] = new_time
-                        heapq.heappush(heap, (new_time, to_loc))
-        return 0
-
-
+    while heap:
+        curr_time, curr_loc = heapq.heappop(heap)
+        if curr_loc not in visited:
+            visited.add(curr_loc)
+            if curr_loc == endNode:
+                return curr_time
+            for time, to_loc in routes.get(curr_loc, ()):
+                if to_loc in visited:
+                    continue
+                prev = time_dict.get(to_loc, -1 )
+                new_time = curr_time + time
+                if prev == -1 or new_time < prev:
+                    time_dict[to_loc] = new_time
+                    heapq.heappush(heap, (new_time, to_loc))
+    return 0
+start = "S0"
+end = "S4"
+paths = [
+    ["S0", "S1", 16 ],
+    ["S0", "S2", 6],
+    ["S1", "S3", 8],
+    ["S2", "S3", 9],
+    ["S2", "S4", 3],
+    ["S1", "S4", 106],
+    ["S3", "S4", 1]
+]
+print(get_shortest_time(start, end, paths))
