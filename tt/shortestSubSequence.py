24ef4b1d4724a8c010acc0f0f3e21693b8777e0c
Sophia Zhang <atszhang@gmail.com>
2023-01-22 14:47:44 -0500

    minor update

--- tt/shortestSubSequence.py
+++ tt/shortestSubSequence.py
@@ -1,3 +1,4 @@
-# Find the shortest subsequence of array that can be divided by K
+# Find the shortest subsequence of array whose sum can be divided by K
+# subsequence of array are formed by continous items in the array
 # If no such subsequence , return empty list
-
+# https://leetcode.com/problems/subarray-sums-divisible-by-k/solution/
