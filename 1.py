fde0427a936cff6f394ada730af6f03a469d5054
Sophia Zhang <atszhang@gmail.com>
2023-03-12 11:09:28 -0400

    minor update

--- 1.py
+++ 1.py
@@ -427,9 +427,9 @@ print(names.count("Besim"))  # 2
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 slicing = slice(-4, None)
 # Getting the last 3 elements from the list
-print(my_list[slicing])  # [4, 5, 6]
+print(my_list[slicing])  # [7,8,9,10]
 # Getting only the third element starting from the right
-print(my_list[-3])  # 4
+print(my_list[-3])  # 8
 
 
 
