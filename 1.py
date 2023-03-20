e967db872b349cb642f599d0e9e5891d1151f783
Sophia Zhang <atszhang@gmail.com>
2023-03-20 16:33:58 -0400

    minor update

--- 1.py
+++ 1.py
@@ -488,6 +488,12 @@ first_set = {4, 5, 6}
 second_set = {1, 2, 3}
 
 print(first_set.union(second_set))  # {1, 2, 3, 4, 5, 6}
+first_set = {1, 2, 3}
+second_set = {4, 5, 6}
+
+first_set.update(second_set)
+
+print(first_set)  # {1, 2, 3, 4, 5, 6}
 
 def is_positive(number):
     print("Positive" if number > 0 else "Negative")  # Positive
