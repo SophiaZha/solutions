ebb9d19fba8a8a5e4b26e1d5644eb3f101b93379
Sophia Zhang <atszhang@gmail.com>
2023-04-12 15:07:35 -0400

    minor update, added solution

--- 104_maxDepthBinaryTree.py
+++ 104_maxDepthBinaryTree.py
@@ -94,6 +94,25 @@ class Solution:
 
         return res
 
+
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        stack = []
+        if root is not None:
+            stack.append((1, root))
+
+        depth = 0
+        while stack != []:
+            current_depth, root = stack.pop()
+            if root is not None:
+                depth = max(depth, current_depth)
+                stack.append((current_depth + 1, root.left))
+                stack.append((current_depth + 1, root.right))
+
+        return depth
 s = Solution()
 r = TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))
 print(s.maxDepth(r))
