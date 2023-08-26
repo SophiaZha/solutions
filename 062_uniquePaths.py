b0f57f39d386c36bda54555b87505723deb127a9
Sophia Zhang <atszhang@gmail.com>
2023-08-26 12:41:22 -0400

    new solution added

--- 062_uniquePaths.py
+++ 062_uniquePaths.py
@@ -16,6 +16,21 @@ class Solution:
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
 
         return dp[m - 1][n - 1]
+
+    def uniquePath1(self, m: int, n: int)-> int:
+        row = [1] * n
+        for i in range(1, m):
+            newRow = [1] * n
+            for j in range( 1, n):
+                newRow[j] = newRow[j-1] + row[j]
+            row = newRow
+        return row[n-1]
+
+sol = Solution()
+print(sol.uniquePath1(7, 3))
+print(sol.uniquePaths(7, 3))
+
+
 """
 62. Unique Paths
 Medium
