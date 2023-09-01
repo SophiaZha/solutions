ecda5ed63888a0a7d1166cf35113edabfdc52644
Sophia Zhang <atszhang@gmail.com>
2023-08-31 21:35:43 -0400

    small change to simplify

--- 746_minCostClimbingStairs.py
+++ 746_minCostClimbingStairs.py
@@ -3,10 +3,9 @@ from typing import List
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         cost.append(0)
-        for i in reversed(range(len(cost) - 3)):
-            cost[i] = cost[i] + min(cost[i + 1], cost[i + 2])
-        res = min(cost[0], cost[1])
-        return res
+        for i in range(len(cost) -3, -1, -1):
+            cost[i] = cost[i] + min(cost[i+1], cost[i+2])
+        return min(cost[0],cost[1])
 
     def minCostClimbingStairsL(self, cost: List[int]) -> int:
         n = len(cost)
