9d199257646c32db7327cb7c728940759adb1808
Sophia Zhang <atszhang@gmail.com>
2023-09-04 10:48:34 -0400

    added new solutions

--- 271_encodeAndDecodeStrings.py
+++ 271_encodeAndDecodeStrings.py
@@ -15,6 +15,31 @@ class Solution:
             res.append(str[j+1:j+1 +length])
             i = j + 1 + length
         return res
+
+    def encodeL(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        return ''.join(map(lambda s: str(len(s)) + '#' + s, strs))
+
+    def decodeL(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        res = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != '#':
+                j += 1
+            wordlen = int(s[i: j])
+            i = j + 1
+            j = i + wordlen
+            res.append(s[i:j])
+            i = j
+        return res
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
 strs = ["lint","code","love","you"]
 so = Solution()
 str = so.encode(strs)
