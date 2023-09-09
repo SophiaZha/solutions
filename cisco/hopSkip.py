4fe5b6995adc3c636f0e76608e108fcb8833d840
Sophia Zhang <atszhang@gmail.com>
2023-09-09 15:19:29 -0400

    correct the solution

--- cisco/hopSkip.py
+++ cisco/hopSkip.py
@@ -1,50 +1,60 @@
 from typing import List
-class Solution:  # half cooked
-    def getHopSkip(self, matrix: List[List[int]])-> int:
-        rows, cols = len(matrix), len(matrix[0])
-        visited = []
-        skipped = []
-        directions = [ [0, 1], [1, 0], [0, -1], [-1, 0] ]
-        nextDirect = 0
-        start = [0,0]
-        visited.append(start)
-        for i in range(rows):
-            for j in range(cols):
-                for k in range(rows -1, -1 , -1):
-                    for m in range(cols -1, -1 , -1):
-                        pass
+
+
+class Solution:
+    # r, c = map(int, input().split())
+    # matrix = []
+    # for i in range(r):
+    #     matrix.append([int(j) for j in input().split()])
+    def getHopSkip(self, matrix: List[List[int]]) -> int:
+        r, c = len(matrix), len(matrix[0])
+        # matrix = [
+        #     [29, 8, 37],
+        #     [15, 41, 3],
+        #     [1, 10, 14]
+        # ]
+        visited = [[False] * c for _ in range(r)]
+        currR, currC = 0, 0
+        dxdy = ((1, 0), (0, 1), (-1, 0), (0, -1))
+        index = 0
+        visited[currR][currC] = True
         while True:
-            tmpPoint = start + directions[nextDirect]
-            adjustDirect = (nextDirect + 1) % 4
-            if tmpPoint not in visited and tmpPoint[0] in range(rows) and tmpPoint[1] in range(cols):
-                skipped.append(tmpPoint)
-            else:
-                tmpPoint = start + directions[nextDirect ]
+            for i in range(2):
+                nextR, nextC = currR + dxdy[index][0], currC + dxdy[index][1]
+                tempR, tempC = currR, currC
+                if nextR == r or nextC == c or nextR == -1 or nextC == -1 or visited[nextR][nextC]:
+                    index += 1
+                    index %= 4
+                    currR += dxdy[index][0]
+                    currC += dxdy[index][1]
+                else:
+                    currR, currC = nextR, nextC
+                if visited[currR][currC]:
+                    if i == 0:
+                        return matrix[tempR][tempC]
+                    else:
+                        return matrix[tempR - dxdy[index - 1][0]][tempC - dxdy[index - 1][1]]
+                visited[currR][currC] = True
+        return 0
 
-
-            if tmpPoint not in visited and tmpPoint[0] in range(rows) and tmpPoint[1] in range(cols):
-                visited.append(tmpPoint)
-            else:
-                tmpPoint = start + directions[ (nextDirect + 1 ) % 4]
-
-            return 0
 
 so = Solution()
-r, c = map(int, input().split())
-matrix = []
-for i in range(r):
-    matrix.append([int(j) for j in input().split()])
-
-matrix = [
-    [29, 8, 37],
-    [15, 41, 3],
-    [1, 10, 14]
+# r, c = map(int, input().split())
+# matrix = []
+# for i in range(r):
+#     matrix.append([int(j) for j in input().split()])
+matrix1 = [
+    [29, 8, 1],
+    [15, 41, 2],
+    [25, 51, 4],
+    [65, 61, 3],
+    [75, 71, 73]
 ]
-
-# 3 3
-# 29 8 37
-# 15 41 3
-# 1 10 14
-
-
-
+# matrix1 = [
+#     [29, 8, 37, 0, 5],
+#     [15, 42, 3, 9, 2],
+#     [1, 10, 14, 12, 40],
+#     [59, 58, 57, 50, 55],
+#     [61, 62, 63, 64, 65],
+# ]
+print(so.getHopSkip(matrix1))
