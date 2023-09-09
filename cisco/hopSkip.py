4b348685f8f4c619a2c2c9e803678c4e0fee1843
Sophia Zhang <atszhang@gmail.com>
2023-09-09 13:34:16 -0400

    added input handling

--- cisco/hopSkip.py
+++ cisco/hopSkip.py
@@ -8,33 +8,32 @@ class Solution:  # half cooked
         nextDirect = 0
         start = [0,0]
         visited.append(start)
+        for i in range(rows):
+            for j in range(cols):
+                for k in range(rows -1, -1 , -1):
+                    for m in range(cols -1, -1 , -1):
+                        pass
         while True:
             tmpPoint = start + directions[nextDirect]
             adjustDirect = (nextDirect + 1) % 4
             if tmpPoint not in visited and tmpPoint[0] in range(rows) and tmpPoint[1] in range(cols):
                 skipped.append(tmpPoint)
             else:
-                tmpPoint = start + directions[ ]
-            nextDirect =     (nextDirect + 1 ) % 4
+                tmpPoint = start + directions[nextDirect ]
+
 
             if tmpPoint not in visited and tmpPoint[0] in range(rows) and tmpPoint[1] in range(cols):
                 visited.append(tmpPoint)
             else:
                 tmpPoint = start + directions[ (nextDirect + 1 ) % 4]
 
+            return 0
 
-
-
-
-
-
-
-
-
-
-        return 0
-
-
+so = Solution()
+r, c = map(int, input().split())
+matrix = []
+for i in range(r):
+    matrix.append([int(j) for j in input().split()])
 
 matrix = [
     [29, 8, 37],
