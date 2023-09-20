bf8409e13c90f7d98db20309302e6c8e836889de
Sophia Zhang <atszhang@gmail.com>
2023-09-20 12:32:30 -0400

    refactor solution

--- 2023acap/profits.py
+++ 2023acap/profits.py
@@ -1,53 +1,41 @@
 import collections
 import datetime
-
+import time
 class Solution:
     def calculate_max_profit(self,input_data):
-        stocks = set()
-        dates = []
-        max_profit = 0
-        matrix = []
+        dates, matrix, segment_profit_ratio = set(), [] ,[]
         stock_details = collections.defaultdict(list)
-        date_price_ratio = []
-
         for i, row in enumerate(input_data):
             symbol, date, price = row.split(',')
-            stocks.add(symbol)
             price = float(price)
             date = int(datetime.datetime.strptime(date, "%m/%d/%Y").strftime("%Y%m%d"))
             matrix.append([symbol, date, price])
-            print(matrix[i])
-        print("-----------------------")
-        matrix = sorted(matrix, key=lambda x: (x[1], x[0]))
         for (symbol, date, price) in matrix:
-            if date not in dates:
-                dates.append(date)
             stock_details[symbol].append([date, price])
-        for s in stocks:
-            for i, (date, price) in enumerate(stock_details[s]):
+        for s in stock_details.keys():
+            sorted_date_price = sorted(stock_details[s], key=lambda x: x[0] )
+            for i, (date, price) in enumerate(sorted_date_price):
                 if i == 0:
                     pre_price = price
                     pre_date = date
                 else:
                     scale = price/pre_price
                     if scale > 1:
-                        date_price_ratio.append([pre_date, date, scale])
+                        segment_profit_ratio.append([pre_date, date, scale])
                     pre_date = date
                     pre_price = price
-        print(" date price ratio....")
-        date_price_ratio = sorted(date_price_ratio, key= lambda x: ( x[0], x[1] ))
-        for x in date_price_ratio:
+        print("segment profit ratio....")
+        segment_profit_ratio = sorted(segment_profit_ratio, key= lambda x: (x[0], x[1]))
+        for x in segment_profit_ratio:
             print(x)
-        date_end_scale = collections.defaultdict(int)
-        date_end_scale[date_price_ratio[0][0]] = 1
-        date_end_scale[date_price_ratio[0][1]] = date_price_ratio[0][2]
-        res = date_end_scale[date_price_ratio[0][1]]
-        for i in range(1, len(date_price_ratio) ):
-            curr_max_ratio_at_segment_start = max({date_end_scale[key] for key in date_end_scale if key <= date_price_ratio[i][0]})
-            # subset_values = {date_end_scale[key] for key in date_end_scale if key <= 20221522}
-            # curr_max_ratio_at_segment_start = max({date_end_scale[key] for key in date_end_scale if key <= 20221522 })
-            date_end_scale[date_price_ratio[i][1]] = curr_max_ratio_at_segment_start * date_price_ratio[i][2]
-            res = max(res, date_end_scale[date_price_ratio[i][1]] )
+        date_profit_ratio = collections.defaultdict(int)
+        date_profit_ratio[segment_profit_ratio[0][0]] = 1
+        date_profit_ratio[segment_profit_ratio[0][1]] = segment_profit_ratio[0][2]
+        res = date_profit_ratio[segment_profit_ratio[0][1]]
+        for i in range(1, len(segment_profit_ratio)):
+            curr_max_ratio_at_segment_start = max({date_profit_ratio[key] for key in date_profit_ratio if key <= segment_profit_ratio[i][0]})
+            date_profit_ratio[segment_profit_ratio[i][1]] = curr_max_ratio_at_segment_start * segment_profit_ratio[i][2]
+            res = max(res, date_profit_ratio[segment_profit_ratio[i][1]])
         return round(1000.0 * (res -1 ))
 
 so = Solution()
@@ -63,8 +51,11 @@ input_data2 = ["G,12/19/2023,112.19","G,12/01/2023,116.41", "G,12/07/2023,111.36
               "I,12/18/2023,134.07", "I,12/01/2023,132.05", "I,12/03/2023,135.19",
               "A,12/01/2023,187.19", "A,12/04/2023,164.33", "A,12/20/2023,180.94", "A,12/21/2023,179.65"]
 
-# Calculate max profit
+start = time.perf_counter()
 max_profit = so.calculate_max_profit(input_data)
 print("Maximum profit:", max_profit)
 max_profit2 = so.calculate_max_profit(input_data2)
 print("Maximum profit 2:", max_profit2)
+end = time.perf_counter()
+ms = (end - start) * 10 ** 3
+print(f"Elapsed {ms:.03f} mill secs.")
