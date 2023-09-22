9e0df4017629b09caa8923080e62144ace460965
Sophia Zhang <atszhang@gmail.com>
2023-09-22 14:40:14 -0400

    add new solution

--- 079_wordSearch.py
+++ 079_wordSearch.py
@@ -1,6 +1,4 @@
 from typing import List
-
-
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         ROWS, COLS = len(board), len(board[0])
@@ -30,6 +28,32 @@ class Solution:
                 if dfs(r, c, 0):
                     return True
         return False
+
+    def existL(self, board: List[List[str]], word: str) -> bool:
+        rows, cols = len(board), len(board[0])
+        visit = set()
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+            if (r < 0 or r >= rows or c < 0 or c >= cols or
+                    word[i] != board[r][c] or (r, c) in visit
+            ):
+                return False
+            visit.add((r, c))
+            for (x, y) in ((r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)):
+                if dfs(x, y, i + 1) : return True
+            visit.remove((r, c))
+            return False
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0):
+                    return True
+        return False
+board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+word = "ABCCED"
+so = Solution()
+print(so.existL(board, word))
+
 
 
 """
