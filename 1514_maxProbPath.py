057b2ff8a754a6db7a7024a3c36bb0378816fe18
Sophia Zhang <atszhang@gmail.com>
2023-11-03 12:01:03 -0400

    added solution

---
+++ 1514_maxProbPath.py
@@ -0,0 +1,70 @@
+import collections
+import heapq
+from typing import List
+
+
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+        adj = collections.defaultdict(list)
+        i, visit = 0, set()
+        for src, des in edges:
+            adj[src].append([succProb[i], des])
+            adj[des].append([succProb[i], src])
+            i += 1
+        minH = [(-1, start_node)]
+        while minH:
+            (prob, node) = heapq.heappop(minH)
+            visit.add(node)
+            if node == end_node:
+                return -1 * prob
+            for rate, nei in adj[node]:
+                if nei not in visit:
+                    heapq.heappush(minH, (rate*prob, nei) )
+        return 0
+'''
+1514. Path with Maximum Probability
+Medium
+3K
+67
+Companies
+You are given an undirected weighted graph of n nodes (0-indexed), represented by an edge list where edges[i] = [a, b] is an undirected edge connecting the nodes a and b with a probability of success of traversing that edge succProb[i].
+
+Given two nodes start and end, find the path with the maximum probability of success to go from start to end and return its success probability.
+
+If there is no path from start to end, return 0. Your answer will be accepted if it differs from the correct answer by at most 1e-5.
+
+ 
+
+Example 1:
+
+
+
+Input: n = 3, edges = [[0,1],[1,2],[0,2]], succProb = [0.5,0.5,0.2], start = 0, end = 2
+Output: 0.25000
+Explanation: There are two paths from start to end, one having a probability of success = 0.2 and the other has 0.5 * 0.5 = 0.25.
+Example 2:
+
+
+
+Input: n = 3, edges = [[0,1],[1,2],[0,2]], succProb = [0.5,0.5,0.3], start = 0, end = 2
+Output: 0.30000
+Example 3:
+
+
+
+Input: n = 3, edges = [[0,1]], succProb = [0.5], start = 0, end = 2
+Output: 0.00000
+Explanation: There is no path between 0 and 2.
+ 
+
+Constraints:
+
+2 <= n <= 10^4
+0 <= start, end < n
+start != end
+0 <= a, b < n
+a != b
+0 <= succProb.length == edges.length <= 2*10^4
+0 <= succProb[i] <= 1
+There is at most one edge between every two nodes.
+'''
